305f348079cf791c445759d2b1cef9e98b61f56b
Alexei <alex.traihel@gmail.com>
2020-01-30 18:25:03 +0300

    Some fixes

--- core/src/main/java/aemapp/core/models/SearchComponentModel.java
+++ core/src/main/java/aemapp/core/models/SearchComponentModel.java
@@ -2,13 +2,11 @@ package aemapp.core.models;
 
 import aemapp.core.SearchType;
 import aemapp.core.service.SearchService;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
-import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 import javax.inject.Inject;
 import java.util.Set;
 
@@ -18,31 +16,15 @@ public class SearchComponentModel {
     @Inject
     private SearchService searchService;
 
-    @ValueMapValue(
-            name = "text",
-            injectionStrategy = InjectionStrategy.OPTIONAL
-    )
-    @Default(
-            values = {"Text"}
-    )
+    @Inject
     private String text;
 
-    @ValueMapValue(
-            name = "path",
-            injectionStrategy = InjectionStrategy.OPTIONAL
-    )
-    @Default(
-            values = {"Path"}
-    )
+    @Inject
+    @Default(values = "\\")
     private String path;
 
-    @ValueMapValue(
-            name = "type",
-            injectionStrategy = InjectionStrategy.OPTIONAL
-    )
-    @Default(
-            values = {"QUERY_BUILDER"}
-    )
+    @Inject
+    @Default(values = "QUERY_BUILDER")
     private String type;
 
     private Set<String> paths;
